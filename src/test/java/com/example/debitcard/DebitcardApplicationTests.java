3f18a1d557a0fc0fa422d0e719563b28f291f5e8
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-26 13:08:27 -0500

    MIcroservico para Tarjeta de Debito

---
+++ src/test/java/com/example/debitcard/DebitcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.debitcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
