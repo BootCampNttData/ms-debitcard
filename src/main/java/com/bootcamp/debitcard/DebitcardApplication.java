a70ebc70fa443ed223cf5f2521810dfbf3f1052a
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-05-01 04:02:28 -0500

    Update Microservicios

--- src/main/java/com/bootcamp/debitcard/DebitcardApplication.java
+++ src/main/java/com/bootcamp/debitcard/DebitcardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DebitcardApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("jdk.tls.client.protocols","TLSv1.2");
 		SpringApplication.run(DebitcardApplication.class, args);
 	}
 
